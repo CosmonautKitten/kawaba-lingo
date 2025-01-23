787588c2cce113591d085c2bf867fa547a69e8f9
CosmonautKitten <james@maple.id.au>
2025-01-23 13:44:15 +1100

    Update

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -8,57 +8,13 @@ export const Footer = () => {
       <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
         <Button size="lg" variant="ghost" className="w-full cursor-default">
           <Image
-            src="/hr.svg"
-            alt="Croatian"
+            src="/kwb.svg"
+            alt="kawaba"
             height={32}
             width={40}
             className="mr-4 rounded-md"
           />
-          Croatian
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/es.svg"
-            alt="Spanish"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Spanish
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/fr.svg"
-            alt="French"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          French
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/it.svg"
-            alt="Italian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Italian
-        </Button>
-
-        <Button size="lg" variant="ghost" className="w-full cursor-default">
-          <Image
-            src="/jp.svg"
-            alt="Japanese"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          />
-          Japanese
+          kawaba
         </Button>
       </div>
     </div>
